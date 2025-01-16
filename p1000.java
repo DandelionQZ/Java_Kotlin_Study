f0e3eeaf2e39742252fcb1993a9e619f05f785bd
ion <delion.dev@gmail.com>
2025-01-16 23:50:39 +0900

    p1000, p2741

---
+++ p1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class p1000 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        int A = sn.nextInt();
+        int B = sn.nextInt();
+
+        System.out.println(A + B);
+
+        sn.close();
+    }
+}
---
+++ p2741.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p2741 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        int n = sn.nextInt();
+
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i ++;
+        }
+    }
+}
